f1f1de7dc3d884797406ee0bfef9c4e841b47f4e
Sanotsu <1833186810@qq.com>
2020-12-15 16:58:28 +0800

    perf:修改查询经纬度的百度地图API调用,加一层LocalSearch(),修改readme(部分图未补)

--- gpfbm-angular/src/app/app.component.ts
+++ gpfbm-angular/src/app/app.component.ts
@@ -109,6 +109,112 @@ export class AppComponent implements OnInit {
     });
   }
 
+  async testLocalSearch() {
+    // let a = await this.LocalSearch('牌楼街道', '重庆市市辖区万州区', '重庆市,市辖区,万州区', 500101018, 4)
+    // let a = await this.localSearch('海南省黎母山林场（海南黎母山省级自然保护区管理站）',
+    //   '海南省省直辖县级行政区划琼中黎族苗族自治县',
+    //   '海南省,省直辖县级行政区划,琼中黎族苗族自治县', '469030500', '4')
+
+    let a = await this.localSearch('顺化瑶族乡',
+      '贵州省黔东南苗族侗族自治州黎平县',
+      '贵州省,黔东南苗族侗族自治州,黎平县', '469030500', '4')
+
+    console.log(a)
+  }
+
+  /**
+   * 指定父级区域下指定行政地区 查询经纬度
+   * @param areaName 需要查询经纬度的行政区域名称
+   * @param parentArea  需要查询经纬度的行政区域的上一级区域名称
+   * @param mergeParent 是当前指定查询行政地区的所有上级地区拼接的字符串,
+   *            如果在 parentName 内查不到结果,那就用此拼接的值按照某种切割,使用 parentName上级范围去查
+   * @param areaCode 需要查询经纬读的行政区域在pcas-code.json文件的code,不做操作,仅仅最后拼接所用
+   * @param level 当前数据是第几层,也就是行政级别的第几层(省为1,市为2 ...),不做操作,仅仅最后拼接所用
+   */
+  localSearch(areaName, parentArea, mergeParent, areaCode, level): any {
+    return new Promise((resolve, reject) => {
+
+      // // 预设好返回百度地图js API返回手动拼接的数据结构
+      // areaName, parentArea, mergeParent, areaCode, level
+      let areaObj = {
+        name: areaName,
+        code: areaCode,
+        level,
+        mergeName: mergeParent + ',' + areaName,
+        lng: 0,
+        lat: 0,
+        info: '',
+        tag: ''
+      };
+      try {
+        const options = {
+          onSearchComplete: async (result) => {
+            /*
+            StatusCode
+            此常量用于描述对象当前状态。
+            常量	描述
+            BMAP_STATUS_SUCCESS	检索成功。对应数值 0 
+            BMAP_STATUS_CITY_LIST	城市列表。对应数值 1 
+            BMAP_STATUS_UNKNOWN_LOCATION	位置结果未知。对应数值 2 
+            BMAP_STATUS_UNKNOWN_ROUTE	导航结果未知。对应数值 3 
+            BMAP_STATUS_INVALID_KEY	非法密钥。对应数值 4 
+            BMAP_STATUS_INVALID_REQUEST	非法请求。对应数值 5 
+            BMAP_STATUS_PERMISSION_DENIED	没有权限。对应数值 6 
+            BMAP_STATUS_SERVICE_UNAVAILABLE	服务不可用。对应数值 7 
+            BMAP_STATUS_TIMEOUT	超时。对应数值 8 
+            */
+
+            // 判断状态是否正确
+            if (local.getStatus() == '0') {
+              // 检索POI数据是设定了显示数量的,不管设的多少,此处都预设认为第一个是最符合结果的,也是我们最后存入文件的哪一个
+              // for (var i = 0; i < result.getCurrentNumPois(); i++) {
+              //   console.log(result.getPoi(i));
+              // }
+
+              let temp = result.getPoi(0)
+
+              // 能够直接查到经纬度的结果
+              areaObj.lng = temp.point.lng;
+              areaObj.lat = temp.point.lat;
+              areaObj.info = '直接检索POI第一个结果成功!';
+              areaObj.tag = temp.tags || '';
+              resolve({ area: areaObj });
+            } else {
+
+              let msg = `检索POI失败,BMAP_STATUE_CODE:${local.getStatus()}`
+              // areaObj.info = msg;
+              // resolve({ noLngLat: areaObj })
+
+              let obj = await this.getPoint(areaName, parentArea, mergeParent, areaCode, level)
+
+              obj.area.info = msg + ' ,GetPoint结果: ' + obj.area.info
+
+              resolve(obj)
+
+            }
+          }, pageCapacity: 4
+        }
+
+        // let local = new BMap.LocalSearch(parentArea, options);
+
+        // 实测,这个和getPoint相反,不能过度扩大parent,要为上一级才能查到结果
+        // 所以不用整体拼接的结果,仅用上一层(市内查县)或上两层(市县查镇)
+        let tempArr = mergeParent.split(",");
+        let queryParentName = (level === 4) ? tempArr[tempArr.length - 2] : tempArr[tempArr.length - 1];
+        let local = new BMap.LocalSearch(queryParentName, options);
+
+        // 有些乡镇名字很长，还有括号说明，这样是查不出来的，需要删除括号内只保留前面的内容查询才可以
+        let queryName = areaName.split('（' ? '（' : '(')[0]
+        local.search(queryName);
+
+      } catch (error) {
+        console.log(error)
+        resolve('error')
+      }
+
+    });
+  }
+
   /**
    * 逐层递归调用获取指定 区域的经纬度函数
    * @param data json数据源中的数据及其子数组
@@ -129,16 +235,18 @@ export class AppComponent implements OnInit {
         // this.sleep(1000);
         let rst;
         if (!parentName) {
-          rst = await this.getPoint(e.name, e.name, e.name, e.code, level);
+          // rst = await this.getPoint(e.name, e.name, e.name, e.code, level);
+          rst = await this.localSearch(e.name, e.name, e.name, e.code, level);
         } else {
-          rst = await this.getPoint(e.name, parentName, mergeParent, e.code, level);
+          // rst = await this.getPoint(e.name, parentName, mergeParent, e.code, level);
+          rst = await this.localSearch(e.name, parentName, mergeParent, e.code, level);
         }
 
         // 不同结果存入不同内容
         if (rst.noLngLat) {
           this.resultNoLngLatArr.push(rst.noLngLat);
           // 为了保持数据完整,即便没有查询到经纬度,也把该行政区域放进去,只不过经纬度为0而已
-          this.resultAreaArr.push(rst.area);
+          this.resultAreaArr.push(rst.noLngLat);
         }
         if (rst.area) {
           this.resultAreaArr.push(rst.area);
@@ -160,7 +268,7 @@ export class AppComponent implements OnInit {
               newParent = parentName + e.name;
             }
 
-            // 这个mergeParent用于查询时已经只取省份了，所以不用提出同名（城市、区县和“直辖市”）
+            // 这个mergeParent用于查询时已经只取省份了，所以不用提出同名（城市、区县和 直辖市 ）
             newMergeParent = mergeParent ? mergeParent + ',' + e.name : e.name;
 
             await this.getChilds(e.children, level + 1, newMergeParent, newParent);
@@ -178,7 +286,7 @@ export class AppComponent implements OnInit {
     console.time('total');
     // 这个31,其实就是拆分的小pcas-code-*.json文件的数量,31个省,但最好不要一次性就遍历处理31个,崩溃啊,溢出啊,挺麻烦的
     // 三五个一次吧,改动这个for循环就好
-    for (let n = 27; n <= 31; n++) {
+    for (let n = 1; n <= 10; n++) {
 
       try {
         const res = await this.ljs.loadpcas(n).toPromise();
